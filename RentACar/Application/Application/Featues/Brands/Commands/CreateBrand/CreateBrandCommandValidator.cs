3db92a4896339060d626880d3956de7af105addb
Esquetta <esquetta@hotmail.com>
2022-11-06 14:39:53 +0300

    CreateBrand command validator added.

---
+++ RentACar/Application/Application/Featues/Brands/Commands/CreateBrand/CreateBrandCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Featues.Brands.Commands.CreateBrand
+{
+    public class CreateBrandCommandValidator:AbstractValidator<CreateBrandCommand>
+    {
+        public CreateBrandCommandValidator()
+        {
+            RuleFor(x=>x.BrandName)
+                .NotEmpty();
+        }
+    }
+}
