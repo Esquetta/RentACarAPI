daa22d948464c0eeaf6a5e75d35c5b74a23482c0
Esquetta <esquetta@hotmail.com>
2022-11-06 14:32:57 +0300

    Create Brand mapping profile added.

---
+++ RentACar/Application/Application/Featues/Brands/Profiles/Profiles.cs
@@ -0,0 +1,22 @@
+﻿using Application.Featues.Brands.Commands.CreateBrand;
+using Application.Featues.Brands.Dtos;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Featues.Brands.Profiles
+{
+    public class Profiles:Profile
+    {
+        public Profiles()
+        {
+            CreateMap<CreateBrandCommand, Brand>().ReverseMap();
+
+            CreateMap<CreatedBrandDto, Brand>().ReverseMap();
+        }
+    }
+}
