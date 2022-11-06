0ddf6161cbb3a99b863681715389b72c6177941f
Esquetta <esquetta@hotmail.com>
2022-11-06 16:03:11 +0300

    Update brand validator added.

---
+++ RentACar/Application/Application/Featues/Brands/Commands/UpdateBrand/UpdateBrandCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Featues.Brands.Commands.UpdateBrand
+{
+    public class UpdateBrandCommandValidator:AbstractValidator<UpdateBrandCommand>
+    {
+        public UpdateBrandCommandValidator()
+        {
+            RuleFor(x=>x.Id).NotEmpty();
+            RuleFor(x=>x.BrandName).NotEmpty();
+
+
+        }
+    }
+}
