516d1ded3f16a7f453f0246c454149633f2e79d7
Esquetta <esquetta@hotmail.com>
2022-11-06 15:08:48 +0300

    DeletedBrand dto added to dtos.

---
+++ RentACar/Application/Application/Featues/Brands/Dtos/DeletedBrandDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Featues.Brands.Dtos
+{
+    public class DeletedBrandDto
+    {
+        public int Id { get; set; }
+        public string BrandName { get; set; }
+    }
+}
