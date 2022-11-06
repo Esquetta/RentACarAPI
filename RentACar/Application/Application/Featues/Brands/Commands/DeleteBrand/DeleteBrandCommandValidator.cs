fb67913b47c3a8e2702e5e32aaae6d5aed01cc85
Esquetta <esquetta@hotmail.com>
2022-11-06 15:19:47 +0300

    DeleteBrandCommandValidator added.

---
+++ RentACar/Application/Application/Featues/Brands/Commands/DeleteBrand/DeleteBrandCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Featues.Brands.Commands.DeleteBrand
+{
+    public class DeleteBrandCommandValidator:AbstractValidator<DeleteBrandCommand>
+    {
+        public DeleteBrandCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+        }
+                
+    }
+}
