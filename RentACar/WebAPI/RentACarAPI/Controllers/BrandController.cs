a74ae74364ebebea1503e60f57f4501b9d2a5f69
Esquetta <esquetta@hotmail.com>
2022-11-06 16:27:11 +0300

    BrandController created,GetList action added.

---
+++ RentACar/WebAPI/RentACarAPI/Controllers/BrandController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Featues.Brands.Models;
+using Application.Featues.Brands.Queries.GetListLBrand;
+using Core.Application.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RentACarAPI.Controllers
+{
+    [Route("api/[controller]/actions")]
+    [ApiController]
+    public class BrandController : BaseController
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListBrandQuery getListBrandQuery = new() { PageRequest = pageRequest };
+            BrandListViewModel result = await Mediator.Send(getListBrandQuery);
+            return Ok(result);
+        }
+    }
+}
