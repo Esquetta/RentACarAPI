06b00feab45f9732f0963d11c535b7363cf69990
Esquetta <esquetta@hotmail.com>
2022-11-06 16:05:03 +0300

    BrandListview model added.

---
+++ RentACar/Application/Application/Featues/Brands/Models/BrandListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Featues.Brands.Models
+{
+    public class BrandListViewModel
+    {
+        public List<BrandListViewDto> Items { get; set; }
+    }
+}
