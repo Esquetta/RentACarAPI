ba9cfedc05eecb7c6288d2eb6e3efc64c265f424
Esquetta <esquetta@hotmail.com>
2022-11-06 16:20:58 +0300

    GetListBrandQuery added.

--- RentACar/Application/Application/Featues/Brands/Queries/GetListLBrand/GetListBrandQuery.cs
+++ RentACar/Application/Application/Featues/Brands/Queries/GetListLBrand/GetListBrandQuery.cs
@@ -1,4 +1,10 @@
-﻿using MediatR;
+﻿using Application.Featues.Brands.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +13,29 @@ using System.Threading.Tasks;
 
 namespace Application.Featues.Brands.Queries.GetListLBrand
 {
-    public class GetListBrandQuery:IRequest<>
+    public class GetListBrandQuery:IRequest<BrandListViewModel>
     {
+        public PageRequest  PageRequest { get; set; }
+
+
+        public class GetListBrandQueryHandler:IRequestHandler<GetListBrandQuery,BrandListViewModel>
+        {
+            private readonly IBrandRepository brandRepository;
+            private readonly IMapper mapper;
+            public GetListBrandQueryHandler(IBrandRepository brandRepository, IMapper mapper)
+            {
+                this.brandRepository = brandRepository;
+                this.mapper = mapper;
+            }
+
+            public async Task<BrandListViewModel> Handle(GetListBrandQuery request, CancellationToken cancellationToken)
+            {
+                IPaginate<Brand> paginate = await brandRepository.GetListAsync();
+
+                BrandListViewModel brandListViewModel = mapper.Map<BrandListViewModel>(paginate);
+
+                return brandListViewModel;
+            }
+        }
     }
 }
