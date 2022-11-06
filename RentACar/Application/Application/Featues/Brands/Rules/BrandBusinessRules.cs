963d4d7534241cbce0de9a2713a388ae710d6fc4
Esquetta <esquetta@hotmail.com>
2022-11-06 15:11:43 +0300

    BrandCheckById method  added to BrandBusinessRules.

--- RentACar/Application/Application/Featues/Brands/Rules/BrandBusinessRules.cs
+++ RentACar/Application/Application/Featues/Brands/Rules/BrandBusinessRules.cs
@@ -22,5 +22,10 @@ namespace Application.Featues.Brands.Rules
             Brand brand = await brandRepository.GetAsync(x => x.BrandName == brandName);
             if (brand != null) throw new BusinessException("Brand name exists.");
         }
+        public async Task BrandCheckById(int id)
+        {
+            Brand brand = await brandRepository.GetAsync(x => x.Id==id);
+            if (brand != null) throw new BusinessException("Brand not exists.");
+        }
     }
 }
