acaf011cfde5dc5bb840596e9aff217fe0648a08
Esquetta <esquetta@hotmail.com>
2022-11-06 15:03:08 +0300

    DeleteBrandCommand Added.

---
+++ RentACar/Application/Application/Featues/Brands/Commands/DeleteBrand/DeleteBrandCommand.cs
@@ -0,0 +1,36 @@
+﻿using Application.Featues.Brands.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Featues.Brands.Commands.DeleteBrand
+{
+    public class DeleteBrandCommand:IRequest<DeletedBrandDto>
+    {
+        public int Id { get; set; }
+
+
+        class DeleteBrandCommandHandler:IRequestHandler<DeleteBrandCommand, DeletedBrandDto>
+        {
+            private readonly IBrandRepository brandRepository;
+            private readonly IMapper mapper;
+            private readonly BrandBusinessRules brandBusinessRules;
+            public DeleteBrandCommandHandler(IBrandRepository brandRepository,IMapper mapper,BrandBusinessRules brandBusinessRules)
+            {
+                this.mapper = mapper;
+                this.brandBusinessRules = brandBusinessRules;
+                this.brandRepository = brandRepository;
+            }
+
+            public async Task<DeletedBrandDto> Handle(DeleteBrandCommand request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
