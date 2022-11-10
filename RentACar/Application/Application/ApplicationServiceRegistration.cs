f3fc7da26a25abc3495776869bb9de635ecf5e9b
Esquetta <esquetta@hotmail.com>
2022-11-10 20:17:05 +0300

    Fixing issues

--- RentACar/Application/Application/ApplicationServiceRegistration.cs
+++ RentACar/Application/Application/ApplicationServiceRegistration.cs
@@ -14,16 +14,11 @@ namespace Application
 {
     public static class ApplicationServiceRegistration
     {
-
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-           
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
-
-
             services.AddScoped<BrandBusinessRules>();
-
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehavior<,>));
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>));
--- RentACar/WebAPI/RentACarAPI/Program.cs
+++ RentACar/WebAPI/RentACarAPI/Program.cs
@@ -4,12 +4,9 @@ using Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-
-
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices();
-builder.Services.AddSecurityServices();
+//builder.Services.AddSecurityServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
 //builder.Services.AddInfrastructureServices();
 builder.Services.AddHttpContextAccessor();
