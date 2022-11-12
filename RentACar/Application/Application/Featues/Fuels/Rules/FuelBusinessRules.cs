af69c4e9bf313078c87cefcecf0f8c886f1b9f92
Esquetta <esquetta@hotmail.com>
2022-11-12 22:08:00 +0300

    Fuel business rules added.

---
+++ RentACar/Application/Application/Featues/Fuels/Rules/FuelBusinessRules.cs
@@ -0,0 +1,26 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Featues.Fuels.Rules
+{
+    public class FuelBusinessRules
+    {
+        private readonly IFuelRepository fuelRepository;
+        public FuelBusinessRules(IFuelRepository fuelRepository)
+        {
+            this.fuelRepository = fuelRepository;
+        }
+
+        public async Task FuelCannotBeDuplicatedWhenInserted(string fuelName)
+        {
+            Fuel fuel = await fuelRepository.GetAsync(x=>x.FuelType==fuelName);
+            if (fuel == null) throw new BusinessException("Fuel exist."); 
+        }
+    }
+}
