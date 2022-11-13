29f12d5f355e93a49deed0ba5d59dec1f4433509
Esquetta <esquetta@hotmail.com>
2022-11-13 18:43:20 +0300

    DeleteFuelCommand validator added.

---
+++ RentACar/Application/Application/Featues/Fuels/Commands/DeleteFuel/DeleteFuelCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Featues.Fuels.Commands.DeleteFuel
+{
+    public class DeleteFuelCommandValidator:AbstractValidator<DeleteFuelCommand>
+    {
+        public DeleteFuelCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+        }
+    }
+}
