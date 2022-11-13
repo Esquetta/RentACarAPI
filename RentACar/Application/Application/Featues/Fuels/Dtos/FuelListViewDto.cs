1149a79918e3eb9d7079c8d4b097facbba2f8137
Esquetta <esquetta@hotmail.com>
2022-11-13 19:04:32 +0300

    FuelList view model and dto added.

---
+++ RentACar/Application/Application/Featues/Fuels/Dtos/FuelListViewDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Featues.Fuels.Dtos
+{
+    public class FuelListViewDto
+    {
+        public int Id { get; set; }
+        public string FuelType { get; set; }
+    }
+}
---
+++ RentACar/Application/Application/Featues/Fuels/Models/FuelListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Application.Featues.Fuels.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Featues.Fuels.Models
+{
+    public class FuelListViewModel
+    {
+        public List<FuelListViewDto> Items { get; set; }
+    }
+}
