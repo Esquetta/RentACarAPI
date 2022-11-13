101ad26191b622355d2efbf2975427c7dd4f863e
Esquetta <esquetta@hotmail.com>
2022-11-13 18:54:22 +0300

    UpdateFuelCommandValidator added.

---
+++ RentACar/Application/Application/Featues/Fuels/Commands/UpdateFuel/UpdateFuelCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.Featues.Fuels.Commands.DeleteFuel;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Featues.Fuels.Commands.UpdateFuel
+{
+    public class UpdateFuelCommandValidator:AbstractValidator<UpdateFuelCommand>
+    {
+        public UpdateFuelCommandValidator()
+        {
+            RuleFor(x=>x.FuelType).NotEmpty();
+        }
+    }
+}
