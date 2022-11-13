0100cc561a23b388b039879270cad418d21d3db3
Esquetta <esquetta@hotmail.com>
2022-11-13 19:09:38 +0300

    GetListFuelQuery finished.

--- RentACar/Application/Application/Featues/Fuels/Queries/GetListFuels/GetListFuelQuery.cs
+++ RentACar/Application/Application/Featues/Fuels/Queries/GetListFuels/GetListFuelQuery.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using Application.Featues.Fuels.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +13,30 @@ using System.Threading.Tasks;
 
 namespace Application.Featues.Fuels.Queries.GetListFuels
 {
-    internal class GetListFuelQuery
+    public class GetListFuelQuery:IRequest<FuelListViewModel>
     {
+        public PageRequest  PageRequest { get; set; }
+
+
+        public class GetListFuelQueryHandler:IRequestHandler<GetListFuelQuery, FuelListViewModel>
+        {
+            private readonly IFuelRepository fuelRepository;
+            private readonly IMapper mapper;
+
+            public GetListFuelQueryHandler(IFuelRepository fuelRepository, IMapper mapper)
+            {
+                this.fuelRepository = fuelRepository;
+                this.mapper = mapper;
+            }
+
+            public async Task<FuelListViewModel> Handle(GetListFuelQuery request, CancellationToken cancellationToken)
+            {
+                IPaginate<Fuel> paginate = await fuelRepository.GetListAsync(index: request.PageRequest.Page, size: request.PageRequest.PageSize);
+
+                FuelListViewModel fuelListViewModel = mapper.Map<FuelListViewModel>(paginate);
+
+                return fuelListViewModel;
+            }
+        }
     }
 }
