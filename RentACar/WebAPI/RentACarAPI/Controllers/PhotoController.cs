108fb44f9db530c34db63543a5ab6ec755e360df
Esquetta <esquetta@gmail.com>
2022-12-17 16:27:33 +0300

    Photo controller and actions added.

---
+++ RentACar/WebAPI/RentACarAPI/Controllers/PhotoController.cs
@@ -0,0 +1,35 @@
+﻿using Application.Featues.Photos.Commands.CreatePhoto;
+using Application.Featues.Photos.Commands.DeletePhoto;
+using Application.Featues.Photos.Commands.UpdatePhoto;
+using Application.Featues.Photos.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RentACarAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PhotoController : BaseController
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]CreatePhotoCommand createPhotoCommand)
+        {
+            CreatedPhotoDto createdPhotoDto = await Mediator.Send(createPhotoCommand);
+            return Ok(createdPhotoDto);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdatePhotoCommand updatePhotoCommand)
+        {
+            UpdatedPhotoDto updatedPhotoDto = await Mediator.Send(updatePhotoCommand);
+            return Ok(updatedPhotoDto);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] DeletePhotoCommand deletePhotoCommand)
+        {
+            DeletedPhotoDto deletedPhotoDto = await Mediator.Send(deletePhotoCommand);
+            return Ok(deletedPhotoDto);
+
+        }
+    }
+}
