7c8bd1e73fbfa3c482401543d6393dbf115a19ac
Esquetta <esquetta@gmail.com>
2022-12-18 14:15:10 +0300

    Photo list view dto added.

---
+++ RentACar/Application/Application/Featues/Photos/Dtos/PhotoListViewDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Featues.Photos.Dtos
+{
+    public class PhotoListViewDto
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+        public string PublicId { get; set; }
+    }
+}
