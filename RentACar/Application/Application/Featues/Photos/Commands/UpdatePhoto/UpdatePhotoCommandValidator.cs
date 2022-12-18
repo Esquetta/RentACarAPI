eb964087d4050584e075633e32335efd8af2b3cb
Esquetta <esquetta@gmail.com>
2022-12-18 15:24:31 +0300

    Photo command validators added.

---
+++ RentACar/Application/Application/Featues/Photos/Commands/UpdatePhoto/UpdatePhotoCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Featues.Photos.Commands.UpdatePhoto
+{
+    public class UpdatePhotoCommandValidator:AbstractValidator<UpdatePhotoCommand>
+    {
+        public UpdatePhotoCommandValidator()
+        {
+            RuleFor(x => x.CarId).GreaterThanOrEqualTo(1);
+            RuleFor(x => x.PublicId).NotEmpty();
+            RuleFor(x => x.Url).NotEmpty();
+            RuleFor(x => x.IsMain).NotEmpty();
+            RuleFor(x=>x.Id).NotEmpty();
+        }
+
+    }
+}
