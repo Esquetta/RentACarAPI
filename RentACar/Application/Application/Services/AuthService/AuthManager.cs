494320baddd37249dcbec0d7ec5790709bcc5f40
Esquetta <esquetta@gmail.com>
2022-12-21 20:21:44 +0300

    Auth manager  operations finished.

--- RentACar/Application/Application/Services/AuthService/AuthManager.cs
+++ RentACar/Application/Application/Services/AuthService/AuthManager.cs
@@ -1,5 +1,8 @@
-﻿using Core.Security.Entities;
+﻿using Application.Services.Repositories;
+using Core.Persistence.Paging;
+using Core.Security.Entities;
 using Core.Security.JWT;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,20 +13,35 @@ namespace Application.Services.AuthService
 {
     public class AuthManager : IAuthService
     {
-        
-        public Task<RefreshToken> AddRefreshToken(RefreshToken refreshToken)
+        private readonly IRefreshTokenRepository refreshTokenRepository;
+        private readonly ITokenHelper tokenHelper;
+        private readonly IUserOperationClaimRepository userOperationClaimRepository;
+        public AuthManager(IRefreshTokenRepository refreshTokenRepository, ITokenHelper tokenHelper, IUserOperationClaimRepository userOperationClaimRepository)
         {
-            throw new NotImplementedException();
+            this.refreshTokenRepository = refreshTokenRepository;
+            this.tokenHelper = tokenHelper;
+            this.userOperationClaimRepository = userOperationClaimRepository;
         }
 
-        public Task<AccessToken> CreateAccessToken(User user)
+        public async Task<RefreshToken> AddRefreshToken(RefreshToken refreshToken)
         {
-            throw new NotImplementedException();
+            RefreshToken addedRefreshToken = await refreshTokenRepository.AddAsync(refreshToken);
+            return addedRefreshToken;
         }
 
-        public Task<RefreshToken> CreateRefreshToken(User user, string IpAddress)
+        public async Task<AccessToken> CreateAccessToken(User user)
         {
-            throw new NotImplementedException();
+            IPaginate<UserOperationClaim> userOperationClaims = await userOperationClaimRepository.GetListAsync(x=>x.UserId==user.Id
+            ,include:x=>x.Include(x=>x.OperationClaim));
+            List<OperationClaim> claims = userOperationClaims.Items.Select(x => new OperationClaim {Id=x.OperationClaim.Id,Name=x.OperationClaim.Name }).ToList();
+            AccessToken accessToken = tokenHelper.CreateToken(user, claims);
+            return accessToken;
+        }
+
+        public async Task<RefreshToken> CreateRefreshToken(User user, string IpAddress)
+        {
+            RefreshToken refreshToken = tokenHelper.CreateRefreshToken(user, IpAddress);
+            return refreshToken;
         }
     }
 }
