424c64c4838e9e6d4883d46e95c4b6bca62d6307
Esquetta <esquetta@gmail.com>
2023-03-02 19:31:16 +0300

    Update

--- RentACar/Application/Application/Featues/Auths/Rules/AuthBusinessRules.cs
+++ RentACar/Application/Application/Featues/Auths/Rules/AuthBusinessRules.cs
@@ -1,9 +1,6 @@
-﻿using Application.Services.AuthService;
-using Application.Services.Repositories;
+﻿using Application.Services.Repositories;
 using Core.CrossCuttingConcerns.Exceptions;
 using Core.Security.Entities;
-using Core.Security.Enums;
-using Core.Security.Hashing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,49 +25,6 @@ namespace Application.Featues.Auths.Rules
         {
             User user = await userRepository.GetAsync(x => x.Email == email);
             if (user == null) throw new BusinessException("Invalid username or password.");
-        }
-        public Task UserShouldBeExist(User? user)
-        {
-            if (user == null)
-                throw new BusinessException(AuthBusinessMessage.UserNotFound);
-
-            return Task.CompletedTask;
-        }
-        public Task UserPasswordCheck(User user, string password)
-        {
-            bool result = HashingHelper.VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt);
-
-            if (result)
-                throw new BusinessException(AuthBusinessMessage.InvlaidPassword);
-
-            return Task.CompletedTask;
-        }
-
-        public Task RefreshTokenCheck(RefreshToken? refreshToken)
-        {
-            if (refreshToken == null)
-                throw new BusinessException(AuthBusinessMessage.RefreshTokenNotFound);
-
-            return Task.CompletedTask;
-        }
-        public Task IsRefreshTokenActive(RefreshToken refreshToken)
-        {
-            if (refreshToken.Revoked != null || (refreshToken.Revoked == null && refreshToken.Expires < DateTime.UtcNow))
-                throw new BusinessException(AuthBusinessMessage.RefreshTokenNotActive);
-            return Task.CompletedTask;
-        }
-
-        public Task NoneAuthentiactorCheck(User user)
-        {
-            if (user.AuthenticatorType is not AuthenticatorType.None)
-                throw new BusinessException(AuthBusinessMessage.UserHasAuthenticator);
-            return Task.CompletedTask;
-        }
-        public Task EmailAuthenticatorCheck(EmailAuthenticator emailAuthenticator)
-        {
-            if (emailAuthenticator is null)
-                throw new BusinessException("Email authenticator not found.");
-            return Task.CompletedTask;
         }
     }
 }
--- RentACar/Application/Application/Services/AuthService/AuthBusinessMessage.cs
+++ RentACar/Application/Application/Services/AuthService/AuthBusinessMessage.cs
@@ -20,4 +20,4 @@ namespace Application.Services.AuthService
     }
 
 }
-
+}
