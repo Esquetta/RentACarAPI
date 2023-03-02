50f7ba71698a76d6668714290423957372596f79
Esquetta <esquetta@gmail.com>
2023-03-02 19:22:58 +0300

    Revert "Services added to ioc."
    This reverts commit b467b2f6f9e926d5f259d8a120ef28aaba948fcd.

--- RentACar/Application/Application/Services/AuthService/AuthBusinessMessage.cs
+++ RentACar/Application/Application/Services/AuthService/AuthBusinessMessage.cs
@@ -10,15 +10,10 @@ namespace Application.Services.AuthService
     {
         public const string AuthenticatorCodeSubject = "You have to enter code for login - RentACar";
         public const string InvalidAuthenticatorCode = "Invlaid code.";
-        public const string UserNotFound = "User not found.";
-        public const string InvlaidPassword = "Invlaid password";
-        public const string RefreshTokenNotFound = "Refresh token not found";
-        public const string RefreshTokenNotActive = "Refresh Token not active";
-        public const string UserHasAuthenticator = "User has authenticator";
 
         public static string AuthenticatorCodeTextBody(string authenticatorCode)
             => $"Your Two factor authcode: {authenticatorCode.Substring(startIndex: 0, length: 3)} {authenticatorCode.Substring(3)}";
     }
 
 }
-
+}
